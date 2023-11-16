e6f0f54b150a256e0e73036b920f5111d0f57e8f
Teemu Harju <harju9797@gmail.com>
2023-11-17 00:47:50 +0200

    Added comments.

--- engine/src/containers/darray.h
+++ engine/src/containers/darray.h
@@ -10,25 +10,93 @@ u64 stride = size of each element in bytes
 void* elements
 */
 
+/**
+ * @brief Enumeration representing properties of a dynamic array.
+ */
 enum {
-	DARRAY_CAPACITY,
-	DARRAY_LENGTH,
-	DARRAY_STRIDE,
-	DARRAY_FIELD_LENGTH
+	DARRAY_CAPACITY,           /**< Capacity of the dynamic array. */
+	DARRAY_LENGTH,             /**< Length or size of the dynamic array. */
+	DARRAY_STRIDE,             /**< Stride of the dynamic array. */
+	DARRAY_FIELD_LENGTH        /**< Length of a field in the dynamic array. */
 };
 
+/**
+ * @brief Creates a dynamic array.
+ *
+ * @param length The initial length of the array.
+ * @param stride The stride (size) of each element in the array.
+ * @return A pointer to the created dynamic array.
+ */
 VAPI void* _darray_create( u64 length, u64 stride );
+
+/**
+ * @brief Destroys a dynamic array.
+ *
+ * @param array A pointer to the dynamic array to be destroyed.
+ */
 VAPI void _darray_destroy( void* array );
 
+/**
+ * @brief Gets the value of a specific field in the dynamic array.
+ *
+ * @param array A pointer to the dynamic array.
+ * @param field The index of the field to retrieve.
+ * @return The value of the specified field.
+ */
 VAPI u64 _darray_field_get( void* array, u64 field );
+
+/**
+ * @brief Sets the value of a specific field in the dynamic array.
+ *
+ * @param array A pointer to the dynamic array.
+ * @param field The index of the field to set.
+ * @param value The value to set for the specified field.
+ */
 VAPI void _darray_field_set( void* array, u64 field, u64 value );
 
+/**
+ * @brief Resizes a dynamic array.
+ *
+ * @param array A pointer to the dynamic array to be resized.
+ * @return A pointer to the resized dynamic array.
+ */
 VAPI void* _darray_resize( void* array );
 
+/**
+ * @brief Pushes a value to the end of the dynamic array.
+ *
+ * @param array A pointer to the dynamic array.
+ * @param value_ptr A pointer to the value to be pushed.
+ * @return A pointer to the modified dynamic array.
+ */
 VAPI void* _darray_push( void* array, const void* value_ptr );
+
+/**
+ * @brief Pops a value from the end of the dynamic array.
+ *
+ * @param array A pointer to the dynamic array.
+ * @param dest A pointer to store the popped value.
+ */
 VAPI void _darray_pop( void* array, void* dest );
 
+/**
+ * @brief Pops a value at a specific index from the dynamic array.
+ *
+ * @param array A pointer to the dynamic array.
+ * @param index The index from which to pop the value.
+ * @param dest A pointer to store the popped value.
+ * @return A pointer to the modified dynamic array.
+ */
 VAPI void* _darray_pop_at( void* array, u64 index, void* dest );
+
+/**
+ * @brief Inserts a value at a specific index in the dynamic array.
+ *
+ * @param array A pointer to the dynamic array.
+ * @param index The index at which to insert the value.
+ * @param value_ptr A pointer to the value to be inserted.
+ * @return A pointer to the modified dynamic array.
+ */
 VAPI void* _darray_insert_at( void* array, u64 index, void* value_ptr );
 
 #define DARRAY_DEFAULT_CAPACITY 1
--- engine/src/core/vmemory.h
+++ engine/src/core/vmemory.h
@@ -2,40 +2,94 @@
 
 #include "defines.h"
 
+/**
+ * @brief Enumeration representing memory tags.
+ *
+ * For temporary use. Should be assigned one of the below or have a new tag created.
+ */
 typedef enum memory_tag {
-	// For temporary use. Should be assigned one of the below or have a new tag created.
-	MEMORY_TAG_UNKNOWN,
-	MEMORY_TAG_ARRAY,
-	MEMORY_TAG_DARRAY,
-	MEMORY_TAG_DICT,
-	MEMORY_TAG_RING_QUEUE,
-	MEMORY_TAG_BST,
-	MEMORY_TAG_STRING,
-	MEMORY_TAG_APPLICATION,
-	MEMORY_TAG_JOB,
-	MEMORY_TAG_TEXTURE,
-	MEMORY_TAG_MATERIAL_INSTANCE,
-	MEMORY_TAG_RENDERER,
-	MEMORY_TAG_GAME,
-	MEMORY_TAG_TRANSFORM,
-	MEMORY_TAG_ENTITY,
-	MEMORY_TAG_ENTITY_NODE,
-	MEMORY_TAG_SCENE,
+	MEMORY_TAG_UNKNOWN,             /**< Unknown memory tag. */
+	MEMORY_TAG_ARRAY,               /**< Memory tag for arrays. */
+	MEMORY_TAG_DARRAY,              /**< Memory tag for dynamic arrays. */
+	MEMORY_TAG_DICT,                /**< Memory tag for dictionaries. */
+	MEMORY_TAG_RING_QUEUE,          /**< Memory tag for ring queues. */
+	MEMORY_TAG_BST,                 /**< Memory tag for binary search trees. */
+	MEMORY_TAG_STRING,              /**< Memory tag for strings. */
+	MEMORY_TAG_APPLICATION,         /**< Memory tag for application-related allocations. */
+	MEMORY_TAG_JOB,                 /**< Memory tag for job-related allocations. */
+	MEMORY_TAG_TEXTURE,             /**< Memory tag for texture-related allocations. */
+	MEMORY_TAG_MATERIAL_INSTANCE,   /**< Memory tag for material instance-related allocations. */
+	MEMORY_TAG_RENDERER,            /**< Memory tag for renderer-related allocations. */
+	MEMORY_TAG_GAME,                /**< Memory tag for game-related allocations. */
+	MEMORY_TAG_TRANSFORM,           /**< Memory tag for transformation-related allocations. */
+	MEMORY_TAG_ENTITY,              /**< Memory tag for entity-related allocations. */
+	MEMORY_TAG_ENTITY_NODE,         /**< Memory tag for entity node-related allocations. */
+	MEMORY_TAG_SCENE,               /**< Memory tag for scene-related allocations. */
 
-	MEMORY_TAG_MAX_TAGS
+	MEMORY_TAG_MAX_TAGS             /**< Maximum number of memory tags. */
 } memory_tag;
 
+/**
+ * @brief Initializes the memory system.
+ */
 VAPI void initialize_memory();
+
+/**
+ * @brief Shuts down the memory system.
+ */
 VAPI void shutdown_memory();
 
+/**
+ * @brief Allocates memory with a specified tag.
+ *
+ * @param size The size of the memory to allocate.
+ * @param tag The memory tag.
+ * @return A pointer to the allocated memory.
+ */
 VAPI void* kallocate( u64 size, memory_tag tag );
 
+/**
+ * @brief Frees allocated memory with a specified tag.
+ *
+ * @param block A pointer to the memory block to free.
+ * @param size The size of the memory block.
+ * @param tag The memory tag.
+ */
 VAPI void kfree( void* block, u64 size, memory_tag tag );
 
+/**
+ * @brief Sets a block of memory to zero.
+ *
+ * @param block A pointer to the memory block.
+ * @param size The size of the memory block.
+ * @return A pointer to the zeroed memory block.
+ */
 VAPI void* kzero_memory( void* block, u64 size );
 
+/**
+ * @brief Copies memory from source to destination.
+ *
+ * @param dest A pointer to the destination memory block.
+ * @param source A pointer to the source memory block.
+ * @param size The size of the memory block.
+ * @return A pointer to the destination memory block.
+ */
 VAPI void* kcopy_memory( void* dest, const void* source, u64 size );
 
+/**
+ * @brief Sets a block of memory to a specified value.
+ *
+ * @param dest A pointer to the destination memory block.
+ * @param value The value to set.
+ * @param size The size of the memory block.
+ * @return A pointer to the destination memory block.
+ */
 VAPI void* kset_memory( void* dest, i32 value, u64 size );
 
+/**
+ * @brief Gets a string representation of the memory usage.
+ *
+ * @return A pointer to the string representation of memory usage.
+ */
 VAPI char* get_memory_usage_str();
+
